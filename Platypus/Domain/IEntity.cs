e76511a2e2cd5c3b41340ae207ac6789660a18f3
karl <karl.statz@gmail.com>
2017-03-22 22:10:04 -0500

    adding value and entity object base types

---
+++ Platypus/Domain/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Platypus.Domain
+{
+    public interface IEntity
+    {
+        long Id { get; set; }
+        Guid Key { get; set; }
+    }
+}
---
+++ Platypus/Domain/ValueObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Platypus.Domain
+{
+    public abstract class ValueObject
+    {
+        public virtual bool Equals<T>(IEqualityComparer<T> comparer, T right) where T : ValueObject
+        {
+            return comparer.Equals((T)this, right);
+        }
+    }
+}
